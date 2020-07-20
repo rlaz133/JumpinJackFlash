68b4b990147dc0a799bb1493d92c239f1d7b844c
Rafael de Lazaro <rafaeldelazaro@gmail.com>
2020-07-20 14:09:54 +0200

    Jump movement achieved

--- Javascript/character.js
+++ Javascript/character.js
@@ -2,7 +2,10 @@ class Character{
     constructor(){
         this.x=10
         this.y=538
-        // this.borders
+        // this.width=66
+        // this.height=92
+        // this.right=this.x+this.width;
+        // this.bottom=this.y+this.height;
         this.direction = 'right'
     }
 
--- Javascript/game.js
+++ Javascript/game.js
@@ -1,10 +1,4 @@
 //startGame encompasses all the game functions to make sure they get loaded after clicking on Start.
-//by itself, 
-// let randw = Math.floor(Math.random()*10)
-// let randx = Math.floor(Math.round()*630)
-// console.log(randx, randw)
-
-
 function startGame(){
     let screen = document.getElementById('canvas');
     screen.style.border = '3px solid black';
@@ -17,22 +11,34 @@ function startGame(){
     charpic.src = 'p1_stand.png';
     let goRight = false;
     let goLeft = false;
+    let jump = false;
+    let framejump =0;
+    let fg = new Image();
+    fg.src = 'grass.png';
 
 
     
+    //Main game interval
+    intervalId = setInterval(() => {
+        frame++;
+        drawMain()}, 33)
 
-    intervalId = setInterval(() => {drawMain()}, 33)
 
-
-    //Here is the logic for the character movement
+    //Here is the listener for the character movement
     document.addEventListener('keydown', function(event){
-        if (event.key === 'd'){
+        if (event.key === 'w'){
+            jump = true;
+            framejump = frame;
+        }
+        else if (event.key === 'd'){
             goRight = true;
             goLeft = false
+            char.direction ='right'
         } 
         else if (event.key === 'a'){
             goLeft= true; 
             goRight = false;
+            char.direction = 'left'
         } 
     })
 
@@ -47,44 +53,55 @@ function startGame(){
 
     //Main drawing function that will be updated on the interval
     function drawMain(){
-        clearScreen();
-        frame++;
         generatePlatform(0, 630, 10);
         drawCharacter();
-        if (frame <4) {randomizePlatform()};
+        if (platforms.length <2) {randomizePlatform()};
+    }
+
+
+    //Updates the Player character
+    function drawCharacter(){
+        ctx.clearRect(char.x, char.y, charpic.width,charpic.height);
         characterMove();
+        ctx.drawImage(charpic, char.x, char.y)
     }
 
-    //function to clear the screen every frame
-    function clearScreen(){
-        ctx.clearRect(0, 0, screen.width, screen.height)
+    //Here is the logic for the character movement
+    function characterMove(){
+        if (jump && framejump +30 > frame){
+            if (char.direction === 'right'){
+                char.y-=5
+                if (char.x + charpic.width < screen.width){char.x += 5}
+                else {char.x =0};
+            }
+            else if (char.direction === 'left'){
+                if (char.x > 0){char.x -= 5}
+                else {char.x = screen.width-charpic.width}
+                char.y -= 5;
+            }
+        }
+        if (jump && framejump +30 === frame){
+                jump = false;
+        }
+        if (goRight && !jump){
+            if (char.x + charpic.width < screen.width){char.x += 5}
+            else {char.x =0};
+            char.direction='right';
+        }
+        else if (goLeft && !jump){
+            if (char.x > 0){char.x -= 5}
+            else {char.x = screen.width-charpic.width}
+            char.direction='left';
+        }
     }
 
-    //Draws a platform in the coordinates and of the width specified
+
+    //Creates a platform in the coordinates and of the width specified and pushes it into an array
     function generatePlatform (x, y, width){
-        let fg = new Image();
-        fg.src = 'grass.png';
         for (i=0; i<width; i++){
         ctx.drawImage(fg, x+fg.width*i, y);}
         platforms.push (fg);
     }
-
-    //Draws the Player character
-    function drawCharacter(){
-
-        ctx.drawImage(charpic, char.x, char.y)
-        
-    }
-    function characterMove(){
-        if (goRight && char.x < screen.width - charpic.width){
-            char.x = char.x + 5
-        }
-        else if (goLeft && char.x > 0){
-            char.x = char.x - 5
-        }
-    }
-
-
 
     //Calculates coordinates to feed the generatePlatform function
 
@@ -94,9 +111,4 @@ function startGame(){
         // let randy = Math.floor(Math.random()*400)
         generatePlatform(randx, 30, randw)
     }
-
-    //Moves the character
-    // function characterMove(){
-
-    // }
 }
